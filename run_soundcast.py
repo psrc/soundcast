6ab0f1d82bf3109740e3578ddc30942a7ef16e07
Suzanne Childress <childresssuzanne@gmail.com>
2014-10-07 08:21:18 -0700

    fixed a variable reference that no long exists recipr_sample to pop_sample[iteration]

--- run_soundcast.py
+++ run_soundcast.py
@@ -378,7 +378,7 @@ if should_build_shadow_price:
         copy_shadow = False
         daysim_assignment(iteration, pop_sample[iteration], copy_shadow, 'configuration_template.properties')
 
-        if iteration > 0 & recipr_sample == 1:
+        if iteration > 0 & pop_sample[iteration] == 1:
             con_file = open('inputs/converge.txt', 'r')
             converge = json.load(con_file)
         if converge == 'stop':
@@ -428,7 +428,7 @@ else:
     for iteration in range(0,len(pop_sample)):
         #copy_shadow = False
         daysim_assignment(iteration, pop_sample[iteration], copy_shadow, 'configuration_template.properties')
-        if iteration > 0 & recipr_sample == 1:
+        if iteration > 0 & pop_sample[iteration] == 1:
             con_file = open('inputs/converge.txt', 'r')
             converge = json.load(con_file)
         if converge == 'stop':
