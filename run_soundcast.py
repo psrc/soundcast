d692ad181276237252c4342ebd120d2edd74ed2f
Suzanne Childress <childresssuzanne@gmail.com>
2014-10-07 09:34:57 -0700

    fixed an indenting bug with when to check for convergence

--- run_soundcast.py
+++ run_soundcast.py
@@ -381,8 +381,8 @@ if should_build_shadow_price:
         if iteration > 0 & pop_sample[iteration] == 1:
             con_file = open('inputs/converge.txt', 'r')
             converge = json.load(con_file)
-        if converge == 'stop':
-            print "done"
+            if converge == 'stop':
+                print "done"
             con_file.close()
             break
         print 'keep going'
@@ -431,10 +431,10 @@ else:
         if iteration > 0 & pop_sample[iteration] == 1:
             con_file = open('inputs/converge.txt', 'r')
             converge = json.load(con_file)
-        if converge == 'stop':
-            print "done"
-            con_file.close()
-            break
+            if converge == 'stop':
+                print "done"
+                con_file.close()
+                break
         print 'keep going'
         con_file.close()
         #copy_shadow = False
