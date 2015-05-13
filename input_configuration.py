54e876fa6b15ba25ec2dd25c4845ddea8d066a1c
Suzanne Childress <childresssuzanne@gmail.com>
2015-05-13 12:42:10 -0700

    changing the default number of iterations to run feedback
    seemed excessive to me on my observations of the types of scenarios we typically run
    we don't need to get super tight convergence and then change everything entirely with shadow pricing

--- input_configuration.py
+++ input_configuration.py
@@ -37,9 +37,9 @@ run_create_daily_bank = True
 
 
 # Model iterations, population sampling, log files, etc.
-pop_sample = [10, 5, 1, 1, 1]
+pop_sample = [10, 2, 1]
 # start building shadow prices - only run work locations
-shadow_work = [2, 1, 1, 1]
+shadow_work = [2, 1, 1]
 shadow_con = 10 #%RMSE for shadow pricing to consider being converged
 parcel_decay_file = 'inputs/buffered_parcels.dat' #File with parcel data to be compared to
 # run daysim and assignment in feedback until convergence
