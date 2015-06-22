a9127f0894cf12302cb27394507d2995e09bccbf
Suzanne Childress <childresssuzanne@gmail.com>
2015-06-22 16:31:42 -0700

    when converting to 2010 dollars, check to see if the data has already by factored once

--- scripts/utils/convert_hhinc_2000_2010.py
+++ scripts/utils/convert_hhinc_2000_2010.py
@@ -11,17 +11,22 @@ INCOME_FACTOR_00_10 = 1.26
 hh_file_loc = 'inputs/hh_and_persons.h5'
 
 if not os.path.exists(hh_file_loc):
-        print 'Oops you cannot convert the synthetic population income to 2010 dollars because you are missing                  the household file that should be at ' + hh_file_loc
+        print 'Oops you cannot convert the synthetic population income to 2010 dollars because you are missing the household file that should be at ' + hh_file_loc
 
 hh_file=h5py.File(hh_file_loc, "r+")
 
 hh_set = hh_file['Household']
 hh_incomes = np.asarray(hh_set['hhincome'])
-incomes_2010 = (hh_incomes * INCOME_FACTOR_00_10).astype(int)
 
-# delete the income field so you can overwrite with new incomes
-del hh_set['hhincome']
-hh_file.create_dataset('Household/hhincome', data=incomes_2010, compression = 'gzip')
+if('incomeconverted' in hh_set):
+    print 'The income has already been converted and will not be converted again'
+else:
+    incomes_2010 = (hh_incomes * INCOME_FACTOR_00_10).astype(int)
+    # delete the income field so you can overwrite with new incomes
+    del hh_set['hhincome']
+    hh_file.create_dataset('Household/hhincome', data=incomes_2010, compression = 'gzip')
+    hh_file.create_dataset('Household/incomeconverted',(1,))
+    print 'incomes updated'
 
 hh_file.close()
 
