ed63a2c428742374107031659dc5c7a1f3f9d76f
Stefan Coe <coestefan@gmail.com>
2016-01-25 11:47:59 -0800

    Adding trip tables to daily bank.

--- scripts/summarize/standard/daily_bank.py
+++ scripts/summarize/standard/daily_bank.py
@@ -1,7 +1,6 @@
 import inro.emme.database.emmebank as _emmebank
 import os
-from standard_summary_configuration import *
-from emme_configuration import *
+from input_configuration import *
 import json
 import shutil
 from distutils import dir_util
@@ -18,11 +17,24 @@ keep_atts = ['@type']
 def json_to_dictionary(dict_name):
 
     #Determine the Path to the input files and load them
-    skim_params_loc = os.path.abspath(os.path.join(os.getcwd(),"inputs\\skim_params"))    # Assumes the cwd is @ run_soundcast.py; always run this script from run_soundcast.py
+    skim_params_loc = os.path.abspath(os.path.join(os.getcwd(),"inputs\\skim_params")) 
     input_filename = os.path.join(skim_params_loc,dict_name+'.json').replace("\\","/")
     my_dictionary = json.load(open(input_filename))
 
     return(my_dictionary)
+
+def text_to_dictionary(dict_name):
+
+    input_filename = os.path.join('inputs/skim_params/',dict_name+'.json').replace("\\","/")
+    my_file=open(input_filename)
+    my_dictionary = {}
+
+    for line in my_file:
+        k, v = line.split(':')
+        my_dictionary[eval(k)] = v.strip()
+
+    return(my_dictionary)
+
 
 def create_emmebank(dir_name):
     
@@ -63,13 +75,31 @@ def merge_networks(master_network, merge_network):
 
     return master_network
        
-
+#Use a copy of an existing bank for the daily bank
 copy_emmebank('Banks/7to8', 'Banks/Daily')
-
 daily_emmebank =_emmebank.Emmebank(r'Banks\Daily\emmebank')
 daily_scenario = daily_emmebank.scenario(1002)
 daily_network = daily_scenario.get_network()
 
+matrix_dict = text_to_dictionary('demand_matrix_dictionary')
+uniqueMatrices = set(matrix_dict.values())
+
+################## delete all matrices #################
+
+for matrix in daily_emmebank.matrices():
+    daily_emmebank.delete_matrix(matrix.id)
+   
+################ create new matrices in daily emmebank for trip tables only ##############
+
+for unique_name in uniqueMatrices:
+    daily_matrix = daily_emmebank.create_matrix(daily_emmebank.available_matrix_identifier('FULL')) #'FULL' means the full-type of trip table
+    daily_matrix.name = unique_name
+
+daily_matrix_dict = {}
+for matrix in daily_emmebank.matrices():
+    daily_arr = matrix.get_numpy_data()
+    daily_matrix_dict[matrix.name] = daily_arr
+    print matrix.name
 
 time_period_list = []
 
@@ -80,14 +110,27 @@ for tod, time_period in sound_cast_net_dict.iteritems():
     bank = _emmebank.Emmebank(path)
     scenario = bank.scenario(1002)
     network = scenario.get_network()
+    # Trip  table stuff:
+    for matrix in bank.matrices():
+        if matrix.name in daily_matrix_dict:
+            hourly_arr = matrix.get_numpy_data()
+            daily_matrix_dict[matrix.name] = daily_matrix_dict[matrix.name] + hourly_arr
+  
+
+    # Network stuff:
     if len(time_period_list) == 0:
         daily_network = network
         time_period_list.append(time_period)
     elif time_period not in time_period_list:
-        time_period_list.append(time_period)
+        time_period_list.append(time_period) #this line was repeated below
         daily_network = merge_networks(daily_network, network)
-        time_period_list.append(time_period)
+        time_period_list.append(time_period) #this line was repeated above
 daily_scenario.publish_network(daily_network)
+
+# Write daily trip tables:
+for matrix in daily_emmebank.matrices():
+    matrix.set_numpy_data(daily_matrix_dict[matrix.name])
+
 
 for extra_attribute in daily_scenario.extra_attributes():
     print extra_attribute
@@ -118,7 +161,29 @@ for link in daily_network.links():
         link['@tveh'] = link['@tveh'] + link['@v' + item[:4]]
 daily_scenario.publish_network(daily_network)
 
+######################## Validate results ##########################
+
+zone1 = 100
+zone2 = 100
+
+print 'from zone', zone1, 'to zone', zone2
+for matrix1 in daily_emmebank.matrices():
+    NAME = matrix1.name
+    print NAME
+    print 'daily:' , matrix1.get_numpy_data()[zone1][zone2]
+    a = 0
+    for tod, time_period in sound_cast_net_dict.iteritems():
+        path = os.path.join('banks', tod, 'emmebank')
+        bank = _emmebank.Emmebank(path)
+        for matrix2 in bank.matrices():
+            if matrix2.name == NAME:
+                my_arr = matrix2.get_numpy_data()
+                a += my_arr[zone1][zone2]
+    print 'hourly total:', a
+
+
 print 'done'
+
 
             
 
