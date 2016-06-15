adec6f7d5a0d42c05848180734ec2f7a687ba181
bricegnichols <bricenichols@gmail.com>
2016-06-15 09:46:37 -0700

    truck land use restrictions created from parcel data

--- scripts/trucks/truck_model.py
+++ scripts/trucks/truck_model.py
@@ -138,7 +138,7 @@ def truck_productions():
     #Calculate Productions for 3 truck classes (Origin Matrices are populated)
     for key, value in truck_generation_dict['productions'].iteritems():
         my_project.matrix_calculator(result = value['results'], expression = value['expression'])
-        logfile.write("We're printing the productions part.")
+        # logfile.write("We're printing the productions part.")
 
     # Apply land use restriction for heavy trucks to zones w/ no industrial parcels
     my_project.matrix_calculator(result = 'mohtpro', expression = 'mohtpro * motruck')
@@ -148,13 +148,13 @@ def truck_attractions():
    
     for key, value in truck_generation_dict['attractions'].iteritems():
         my_project.matrix_calculator(result = value['results'], expression = value['expression'])
-        logfile.write("We're printing the attractions part.")
+        # logfile.write("We're printing the attractions part.")
 
     # Apply land use restriction for heavy trucks to zones w/ no industrial parcels
     my_project.matrix_calculator(result = 'mohtatt', expression = 'mohtatt * motruck')
 
     truck_dest_matrices = ['ltatt', 'mtatt', 'htatt']
-    print 'done with truck productions and attractions'
+    # print 'done with truck productions and attractions'
     logfile.write('done with truck productions and attractions')
 
     #Transpose Attractions (Destination Matrices are populated)
@@ -346,19 +346,16 @@ def create_landuse_correction():
     '''Restrict truck trips by land use type.'''
 
     #  Read in the csv files for Parcel ID and Taz and Parcel ID and Use Type for join
-    parcels = pd.read_csv(r'D:\soundcast\soundcast\inputs\buffered_parcels.dat', sep=' ')
+    parcels = pd.read_csv(r'inputs/buffered_parcels.txt', sep=' ')
 
-    # Ideally, we attach a land use code to the urbansim input and filter directly from there
-    # For now, we need to join in parcel land use info
-    parcel_lu = pd.read_csv(r'R:\Craig\Trucks\inputs\TripGen\Base\parcels\parcels_allowable_lu.txt')
-
-    df = parcels.merge(parcel_lu, left_on='parcelid',right_on='parcel_id')
+    # Read in land use type lookup
+    df = parcels.merge(pd.read_csv(r'inputs/lu_type.csv'),left_on='lutype_p',right_on='land_use_type_id')
 
     # List of allowable truck land uses
     truck_uses = ['Agriculture','Fisheries','Forest, harvestable','Forest, protected','Industrial','Military','Mining','Warehousing']
 
     # select truck rows only, using the allowable truck land uses
-    truck_df = df[df["generic_land_use_1"].isin(truck_uses)]
+    truck_df = df[df["description"].isin(truck_uses)]
     # Add a flag for truck allowable field
     truck_df['trucks_allowed_parcel'] = 1
 
@@ -371,7 +368,17 @@ def create_landuse_correction():
     df_taz = pd.DataFrame(df.groupby('taz_p').sum()[['trucks_allowed_parcel']])
     df_taz['trucks_allowed_taz'] = pd.cut(df_taz['trucks_allowed_parcel'], bins=[0,1,df_taz['trucks_allowed_parcel'].max()], labels=[0,1], include_lowest=True)
 
-    return df_taz
+    # Filter out all zero zones and write only zones with allowable land use
+    df_taz = df_taz[df_taz["trucks_allowed_taz"] > 0]
+
+    # add some columns required for emme
+    df_taz['emme'] = ['all: 1' for x in xrange(len(df_taz))]
+    df_taz['na'] = [' ' for x in xrange(len(df_taz))]
+    df_taz['taz'] = df_taz.index
+
+    df_string = " " + df_taz['taz'].astype('str') + " " + df_taz['emme']
+    df_head = pd.DataFrame(['t matrices', 'c Zones with Industrial Use Allowed', 'm matrix=motruck'])
+    pd.concat([df_head, df_string]).to_csv(r'inputs/trucks/trucks.in', index=False, header=False)
 
 def write_summary():
     # Write production and attraction totals
@@ -385,14 +392,14 @@ def write_summary():
 
 
 def main():
-    #my_project = EmmeProject(truck_model_project)
+    create_landuse_correction()
     network_importer(my_project)
     my_project.delete_matrices("ALL")
     place_holder_scalar_matrix()
     create_origin_destination_matrices()
     create_scalar_matrices()
     create_full_matrices()
-    import_emp_matrices()
+    import_emp_matrices() 
     calc_total_households()
     truck_productions()
     truck_attractions()
