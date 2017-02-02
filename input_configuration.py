1ad65bf2c11e58afb37a3a8964249c4c8c93d5b0
bricegnichols <bricenichols@gmail.com>
2017-02-02 14:52:01 -0800

    accidentally turned off some configs

--- input_configuration.py
+++ input_configuration.py
@@ -34,12 +34,12 @@ if not(use_simple_configuration):
     #############################################################
     
     ######Set up:######
-    run_accessibility_calcs = False
-    run_copy_daysim_code = False
-    run_setup_emme_project_folders = False
-    run_setup_emme_bank_folders = False
-    run_copy_large_inputs = False
-    run_import_networks = False
+    run_accessibility_calcs = True
+    run_copy_daysim_code = True
+    run_setup_emme_project_folders = True
+    run_setup_emme_bank_folders = True
+    run_copy_large_inputs = True
+    run_import_networks = True
     ###################
 
     ###### Only one of the following should be Tru!!!!!!######
