5c43d7a454d711a4bb7084bcf5aa7d960d25e19e
Stefan Coe <coestefan@gmail.com>
2017-10-06 13:38:48 -0700

    small fix to run supplemental generation.

--- run_soundcast.py
+++ run_soundcast.py
@@ -159,10 +159,11 @@ def run_truck_supplemental(iteration):
     ###outputs.'''
      if run_supplemental_trips:
          # Only run generation script once - does not change with feedback
-        if iteration == 0 and run_supplemental_generation:
-            returncode = subprocess.call([sys.executable,'scripts/supplemental/generation.py'])
-            if returncode != 0:
-                sys.exit(1)
+        if iteration == 0:
+            if run_supplemental_generation:
+                returncode = subprocess.call([sys.executable,'scripts/supplemental/generation.py'])
+                if returncode != 0:
+                    sys.exit(1)
 
         returncode = subprocess.call([sys.executable,'scripts/supplemental/distribute_non_work_ixxi.py'])
         if returncode != 0:
