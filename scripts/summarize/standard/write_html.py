65ab57abb34869b37ba40863fd04936cd33bce1f
Modeller <Modeller@psrc.local>
2020-03-27 15:48:33 -0700

    updating validation_census nb and adding all new validation nb to write_html

--- scripts/summarize/standard/write_html.py
+++ scripts/summarize/standard/write_html.py
@@ -3,8 +3,9 @@ import nbformat
 from nbconvert.preprocessors import ExecutePreprocessor
 
 def main():
+
     # Create HTML sheets from jupyter notebooks
-    for sheet in ['topsheet','metrics','validation', 'validation_census']:
+    for sheet in ['topsheet','metrics','validation','validation_census','validation_tour']:
         with open("scripts/summarize/notebooks/"+sheet+".ipynb") as f:
                 nb = nbformat.read(f, as_version=4)
         ep = ExecutePreprocessor(timeout=600, kernel_name='python2')
@@ -16,6 +17,8 @@ def main():
         if os.path.exists(r"outputs/"+sheet+".html"):
             os.remove(r"outputs/"+sheet+".html")
         os.rename(r"scripts/summarize/notebooks/"+sheet+".html", r"outputs/"+sheet+".html")
+	
+
 
 if __name__ == '__main__':
     main()
