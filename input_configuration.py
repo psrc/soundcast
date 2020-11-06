53528e5fd931abd8e41c684b77b8275cf052b9dc
Modeller <Modeller@psrc.local>
2020-11-05 17:47:24 -0800

    updating base year network

--- input_configuration.py
+++ input_configuration.py
@@ -6,27 +6,27 @@ import os
 model_year = '2018'
 base_year = '2018'
 landuse_inputs = 'vers2_july2020'
-network_inputs = 'OSM_bike_test'
+network_inputs = 'current_2018'
 soundcast_inputs_dir = 'R:/e2projects_two/SoundCast/Inputs/dev'
 
 ##############################
 # Initial Setup
 ##############################
-run_accessibility_calcs = False
-run_setup_emme_project_folders = False
-run_setup_emme_bank_folders = False
-run_copy_scenario_inputs = False
-run_import_networks = False
+run_accessibility_calcs = True
+run_setup_emme_project_folders = True
+run_setup_emme_bank_folders = True
+run_copy_scenario_inputs = True
+run_import_networks = True
 
 ##############################
 # Model Procedures
 ##############################
-run_skims_and_paths_free_flow = False
+run_skims_and_paths_free_flow = True
 run_skims_and_paths = True
-run_truck_model = False
-run_supplemental_trips = False
-run_daysim = False
-run_summaries = False
+run_truck_model = True
+run_supplemental_trips = True
+run_daysim = True
+run_summaries = True
 
 ##############################
 # Modes and Path Types
