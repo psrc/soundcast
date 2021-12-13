20628bd96bfdac269acde664ce75d53da4ad4fde
Brice Nichols <bnichols@psrc.org>
2021-12-13 11:17:38 -0800

    Update write_html.py
    correcting running order for RTP notebooks to remove bug

--- scripts/summarize/standard/write_html.py
+++ scripts/summarize/standard/write_html.py
@@ -45,7 +45,7 @@ def main():
         dirname = r'outputs/RTP'
         if not os.path.exists(dirname):
             os.makedirs(dirname)
-        for sheet in ['costs','transit','mode_share','access','person','standard','congestion','emissions','freight','travel_time','conformity_analysis']:
+        for sheet in ['costs','transit','mode_share','person','access','standard','congestion','emissions','freight','travel_time','conformity_analysis']:
             dirname = os.path.join(r'outputs/compare/RTP',sheet)
             if not os.path.exists(dirname):
                 os.makedirs(dirname)
