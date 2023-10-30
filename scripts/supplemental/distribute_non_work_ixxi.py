5645c5500d6b489ca5e31b10d13cc86e37800606
stefancoe <coestefan@gmail.com>
2023-10-30 15:01:44 -0700

    Adding fix back in.

--- scripts/supplemental/distribute_non_work_ixxi.py
+++ scripts/supplemental/distribute_non_work_ixxi.py
@@ -67,9 +67,6 @@ def load_matrices_to_emme(trip_table_in, trip_purps, fric_facs, my_project):
 
         for p_a in ['pro', 'att']:
             # Load zonal production and attractions from CSV (output from trip generation)
-            
-            trips = np.array(trip_table_in[purpose + p_a])
-            trips = np.resize(trips, zonesDim)
             trips = np.array(trip_table_in[purpose + p_a].reindex(zones, fill_value=0))
             #code below does not work for GQs because there are only 3700 records in the csv file. Not sure if code above is ideal.
             #trips = np.array(trip_table_in.loc[0:zonesDim - 1][purpose + p_a])    # Less 1 because NumPy is 0-based\
