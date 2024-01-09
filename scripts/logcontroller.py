a343dd5c3d9b86f7764324e5f23fcbfa0d6af29c
stefancoe <coestefan@gmail.com>
2024-01-09 10:46:59 -0800

    Code formatting.

--- scripts/logcontroller.py
+++ scripts/logcontroller.py
@@ -1,57 +1,66 @@
-#Copyright [2014] [Puget Sound Regional Council]
+# Copyright [2014] [Puget Sound Regional Council]
 
-#Licensed under the Apache License, Version 2.0 (the "License");
-#you may not use this file except in compliance with the License.
-#You may obtain a copy of the License at
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 
 #    http://www.apache.org/licenses/LICENSE-2.0
 
-#Unless required by applicable law or agreed to in writing, software
-#distributed under the License is distributed on an "AS IS" BASIS,
-#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#See the License for the specific language governing permissions and
-#limitations under the License.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import logging
+
 # from emme_configuration import main_log_file
 from functools import wraps
 from time import time
 import datetime
 import os, sys, errno
+
 sys.path.append(os.getcwd())
 # os.chdir(r'..')
 import toml
 
 
-network_config = toml.load(os.path.join(os.getcwd(), 'configuration\\network_configuration.toml'))
+network_config = toml.load(
+    os.path.join(os.getcwd(), "configuration\\network_configuration.toml")
+)
 
 
 def setup_custom_logger(name):
     # create dir for main log file if it doesn't exist
     try:
-        os.makedirs('outputs/logs')
+        os.makedirs("outputs/logs")
     except OSError as e:
         if e.errno != errno.EEXIST:
             raise
-    logging.basicConfig(filename=network_config['main_log_file'],format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+    logging.basicConfig(
+        filename=network_config["main_log_file"],
+        format="%(asctime)s %(message)s",
+        datefmt="%m/%d/%Y %I:%M:%S %p",
+    )
     handler = logging.StreamHandler()
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
     logger.addHandler(handler)
     return logger
 
+
 def timed(f):
-  @wraps(f)
-  def wrapper(*args, **kwds):
-    main_logger = logging.getLogger('main_logger')
+    @wraps(f)
+    def wrapper(*args, **kwds):
+        main_logger = logging.getLogger("main_logger")
 
-    start = datetime.datetime.now()
-    main_logger.info(" %s starting" % (f.__name__))
+        start = datetime.datetime.now()
+        main_logger.info(" %s starting" % (f.__name__))
 
-    result = f(*args, **kwds)
+        result = f(*args, **kwds)
 
-    elapsed = datetime.datetime.now() - start
-    main_logger.info("%s took %s" % (f.__name__, str(elapsed)))
-    return result
-  return wrapper
-        
+        elapsed = datetime.datetime.now() - start
+        main_logger.info("%s took %s" % (f.__name__, str(elapsed)))
+        return result
+
+    return wrapper
--- scripts/network/network_importer.py
+++ scripts/network/network_importer.py
@@ -1,241 +1,292 @@
 import pandas as pd
 import os, sys
-import re 
+import re
 import multiprocessing as mp
 import subprocess
 import json
 from multiprocessing import Pool, pool
-sys.path.append(os.path.join(os.getcwd(),"inputs"))
-sys.path.append(os.path.join(os.getcwd(),"scripts"))
+
+sys.path.append(os.path.join(os.getcwd(), "inputs"))
+sys.path.append(os.path.join(os.getcwd(), "scripts"))
 sys.path.append(os.getcwd())
 # from emme_configuration import *
 # from input_configuration import *
 from EmmeProject import *
 import toml
-config = toml.load(os.path.join(os.getcwd(), 'configuration/input_configuration.toml'))
-network_config = toml.load(os.path.join(os.getcwd(), 'configuration/network_configuration.toml'))
-emme_config = toml.load(os.path.join(os.getcwd(), 'configuration/emme_configuration.toml'))
+
+config = toml.load(os.path.join(os.getcwd(), "configuration/input_configuration.toml"))
+network_config = toml.load(
+    os.path.join(os.getcwd(), "configuration/network_configuration.toml")
+)
+emme_config = toml.load(
+    os.path.join(os.getcwd(), "configuration/emme_configuration.toml")
+)
+
 
 def json_to_dictionary(dict_name):
-
-    #Determine the Path to the input files and load them
-    input_filename = os.path.join('inputs/model/skim_parameters/',dict_name+'.json').replace("\\","/")
+    # Determine the Path to the input files and load them
+    input_filename = os.path.join(
+        "inputs/model/skim_parameters/", dict_name + ".json"
+    ).replace("\\", "/")
     my_dictionary = json.load(open(input_filename))
 
-    return(my_dictionary)
-          
+    return my_dictionary
+
+
 def multiwordReplace(text, replace_dict):
     rc = re.compile(r"[A-Za-z_]\w*")
+
     def translate(match):
         word = match.group(0)
         return replace_dict.get(word, word)
+
     return rc.sub(translate, text)
+
 
 def update_headways(emmeProject, headways_df):
     network = emmeProject.current_scenario.get_network()
     for transit_line in network.transit_lines():
         row = headways_df.loc[(headways_df.id == int(transit_line.id))]
-        if int(row['hdw_' + emmeProject.tod]) > 0:
-            transit_line.headway = int(row['hdw_' + emmeProject.tod])
+        if int(row["hdw_" + emmeProject.tod]) > 0:
+            transit_line.headway = int(row["hdw_" + emmeProject.tod])
         else:
             network.delete_transit_line(transit_line.id)
     emmeProject.current_scenario.publish_network(network)
 
+
 def distance_pricing(distance_rate, emmeProject):
-   toll_atts = ["@toll1", "@toll2", "@toll3", "@trkc1", "@trkc2", "@trkc3"]
-   network = emmeProject.current_scenario.get_network()
-   for link in network.links():
+    toll_atts = ["@toll1", "@toll2", "@toll3", "@trkc1", "@trkc2", "@trkc3"]
+    network = emmeProject.current_scenario.get_network()
+    for link in network.links():
         if link.data3 > 0:
-            if config['add_distance_pricing']:
+            if config["add_distance_pricing"]:
                 for att in toll_atts:
                     link[att] = link[att] + (link.length * distance_rate)
-   emmeProject.current_scenario.publish_network(network)
+    emmeProject.current_scenario.publish_network(network)
+
 
 def arterial_delay(emmeProject, factor):
     network = emmeProject.current_scenario.get_network()
-    #if '@rdly' in network.attributes('LINK'):
+    # if '@rdly' in network.attributes('LINK'):
     #    emmeProject.delete_extra_attribute('@rdly')
-    emmeProject.create_extra_attribute('LINK', '@rdly', 'arterial delay', True)
+    emmeProject.create_extra_attribute("LINK", "@rdly", "arterial delay", True)
     network = emmeProject.current_scenario.get_network()
-    attribute_dict = {'arterial_flag' : 'LINK', 
-                      'lane_3to5' : 'LINK', 
-                      'lanecap_3to5' : 'LINK', 
-                      'oneway_arterial_flag' : 'LINK', 
-                      'red' : 'LINK', 
-                      'rdly' : 'LINK',  
-                      'number_arts_entering_int' : 'NODE', 
-                      'sum_arts_lane_3to5_entering_int' : 'NODE',
-                      'sum_arts_lanecap_3to5_entering_int' : 'NODE', 
-                      'number_oneway_arts_entering_int' : 'NODE', 
-                      'cycle' : 'NODE'}
+    attribute_dict = {
+        "arterial_flag": "LINK",
+        "lane_3to5": "LINK",
+        "lanecap_3to5": "LINK",
+        "oneway_arterial_flag": "LINK",
+        "red": "LINK",
+        "rdly": "LINK",
+        "number_arts_entering_int": "NODE",
+        "sum_arts_lane_3to5_entering_int": "NODE",
+        "sum_arts_lanecap_3to5_entering_int": "NODE",
+        "number_oneway_arts_entering_int": "NODE",
+        "cycle": "NODE",
+    }
 
     for name, type in attribute_dict.items():
         network.create_attribute(type, name)
-    
+
     for link in network.links():
         ftype = link.data3
         lanes = link.num_lanes
         lanecap = link.data1
 
-
-    # 1) Set temporary link attributes
+        # 1) Set temporary link attributes
 
         # Temp Link Attribute [arterial_flag] = 1 for arterials (ftype=3,4 or 6)
-        if ftype == 3 or ftype == 4 or ftype == 6: 
+        if ftype == 3 or ftype == 4 or ftype == 6:
             link.arterial_flag = 1
-        else: 
+        else:
             link.arterial_flag = 0
 
         # Temp Link Attribute [lane_3to5] = (minimum of 5 or lanes+2 (range 3-5) wherever arterial_flag=1)
-        if link.arterial_flag == 1: 
-            link.lane_3to5 = min(5, (lanes+2))
+        if link.arterial_flag == 1:
+            link.lane_3to5 = min(5, (lanes + 2))
         else:
             link.lane_3to5 = 0
 
         # Temp Link Attribute [lanecap_3to5] = (minimum of 5*lanecap or (lanes+2)*lanecap wherever arterial_flag=1)
         if link.arterial_flag == 1:
-            link.lanecap_3to5 = min((5*lanecap),((lanes+2)*lanecap))
+            link.lanecap_3to5 = min((5 * lanecap), ((lanes + 2) * lanecap))
         else:
-            link.lanecap_3to5 = 0 
+            link.lanecap_3to5 = 0
 
         # Temp Link Attribute [oneway_arterial_flag] = 1 for oneway arterials (ftype=4)
-        if ftype == 4: 
+        if ftype == 4:
             link.oneway_arterial_flag = 1
         else:
             link.oneway_arterial_flag = 0
 
-
     # 2) Aggregate link attributes to temporary node attributes
 
     for node in network.nodes():
-
-        node.number_arts_entering_int = 0    
+        node.number_arts_entering_int = 0
         node.sum_arts_lane_3to5_entering_int = 0
         node.sum_arts_lanecap_3to5_entering_int = 0
         node.number_oneway_arts_entering_int = 0
-            
-        for link in node.incoming_links():
 
+        for link in node.incoming_links():
             # Temp Node Attribute [number_arts_entering_int] = number of arterial links (arterial_flag = 1) entering the intersection (node --> j-node of link)
-            node.number_arts_entering_int = node.number_arts_entering_int + link.arterial_flag
-                
-            # Temp Node Attribute [sum_arts_lane_3to5_entering_int] = sum of lane_3to5 for arterials (arterial_flag = 1) entering the intersection (node --> j-node of link) 
-            node.sum_arts_lane_3to5_entering_int = node.sum_arts_lane_3to5_entering_int + link.lane_3to5
-        
+            node.number_arts_entering_int = (
+                node.number_arts_entering_int + link.arterial_flag
+            )
+
+            # Temp Node Attribute [sum_arts_lane_3to5_entering_int] = sum of lane_3to5 for arterials (arterial_flag = 1) entering the intersection (node --> j-node of link)
+            node.sum_arts_lane_3to5_entering_int = (
+                node.sum_arts_lane_3to5_entering_int + link.lane_3to5
+            )
+
             # Temp Node Attribute [sum_arts_lanecap_3to5_entering_int] = sum of lanecap_3to5 for arterials (arterial_flag = 1) entering the intersection (node --> j-node of link)
-            node.sum_arts_lanecap_3to5_entering_int = node.sum_arts_lanecap_3to5_entering_int + link.lanecap_3to5
-        
+            node.sum_arts_lanecap_3to5_entering_int = (
+                node.sum_arts_lanecap_3to5_entering_int + link.lanecap_3to5
+            )
+
             # Temp Node Attribute [number_oneway_arts_entering_int] = number of oneway arterial links (ftype=4) entering the intersection (node --> j-node of link)
-            node.number_oneway_arts_entering_int = node.number_oneway_arts_entering_int + link.oneway_arterial_flag
-        
- 
+            node.number_oneway_arts_entering_int = (
+                node.number_oneway_arts_entering_int + link.oneway_arterial_flag
+            )
+
     # 3) Calculate intersection cycle time (permanent node attribute - [cycle] in minutes at all nodes)
 
     for node in network.nodes():
-
         node.cycle = 0.0
 
         for link in node.incoming_links():
-
             # Permanent Node Attribute [cycle] = signal cycle duration in minutes where number_arts_entering_int > 2 or number_oneway_arts_entering_int > 1
-            if (node.number_arts_entering_int > 2) or (node.number_oneway_arts_entering_int > 1):
-                node.cycle = 1.0 + (node.sum_arts_lane_3to5_entering_int / 8.0) * (node.number_arts_entering_int / 4.0)
+            if (node.number_arts_entering_int > 2) or (
+                node.number_oneway_arts_entering_int > 1
+            ):
+                node.cycle = 1.0 + (node.sum_arts_lane_3to5_entering_int / 8.0) * (
+                    node.number_arts_entering_int / 4.0
+                )
             else:
                 node.cycle = 0.0
-              
 
     # 4) Calculate red time (permanent link attribute - [red] in minutes at j-node of every arterial link
 
     for node in network.nodes():
-
         for link in node.incoming_links():
-
             link.red = 0.0
 
-            if node.sum_arts_lanecap_3to5_entering_int > 0.0 and link.data3 != 0 and link.data3 != 5:
-                link.red = 1.2 * node.cycle * (1 - (node.number_arts_entering_int * link.lanecap_3to5) / (2 * node.sum_arts_lanecap_3to5_entering_int))
+            if (
+                node.sum_arts_lanecap_3to5_entering_int > 0.0
+                and link.data3 != 0
+                and link.data3 != 5
+            ):
+                link.red = (
+                    1.2
+                    * node.cycle
+                    * (
+                        1
+                        - (node.number_arts_entering_int * link.lanecap_3to5)
+                        / (2 * node.sum_arts_lanecap_3to5_entering_int)
+                    )
+                )
             else:
                 link.red = 0.0
 
             # Use this to match 4K macro results.  Revise in the future to consider freeway and expressway links that end at signalized intersections.
             if link.data3 == 1 or link.data3 == 2:
                 link.red = 0.0
-        
 
     # 5) Calculate intersection delay factor (permanent link attribute - [rdly] in minutes for every arterial link
-            # restrict to arterials for now - not ul3=0 or 5
+    # restrict to arterials for now - not ul3=0 or 5
 
     for node in network.nodes():
-
         for link in node.incoming_links():
-
             link.rdly = 0.0
 
             if node.cycle:
-                link.rdly = min(1,max(0.2,(link.red * link.red) / (2 * node.cycle)))
+                link.rdly = min(1, max(0.2, (link.red * link.red) / (2 * node.cycle)))
 
-    # 6) Factor rdly by 0.5
-    
+            # 6) Factor rdly by 0.5
+
             link.rdly = link.rdly * factor
-            link['@rdly'] = link.rdly
-            
-            #print link.i_node, link.j_node, link.rdly, link.data3
-            
+            link["@rdly"] = link.rdly
+
+            # print link.i_node, link.j_node, link.rdly, link.data3
+
     for name, type in attribute_dict.items():
-            network.delete_attribute(type, name)
+        network.delete_attribute(type, name)
     emmeProject.current_scenario.publish_network(network)
+
 
 def run_importer(project_name):
     my_project = EmmeProject(project_name)
-    headway_df = pd.read_csv('inputs/scenario/networks/headways.csv')
-    tod_index = pd.Series(range(1,len(network_config['tod_networks'])+1),index=network_config['tod_networks'])
-    for key, value in network_config['sound_cast_net_dict'].items():
+    headway_df = pd.read_csv("inputs/scenario/networks/headways.csv")
+    tod_index = pd.Series(
+        range(1, len(network_config["tod_networks"]) + 1),
+        index=network_config["tod_networks"],
+    )
+    for key, value in network_config["sound_cast_net_dict"].items():
         my_project.change_active_database(key)
         for scenario in list(my_project.bank.scenarios()):
             my_project.bank.delete_scenario(scenario)
-        #create scenario
+        # create scenario
         my_project.bank.create_scenario(1002)
         my_project.change_scenario()
         my_project.delete_links()
         my_project.delete_nodes()
-      
-        my_project.process_modes('inputs/scenario/networks/modes.txt')
-        
-        my_project.process_base_network('inputs/scenario/networks/roadway/' + value + '_roadway.in')
-        
-        my_project.process_shape('inputs/scenario/networks/shape/' + value + '_shape.in')
-        my_project.process_turn('inputs/scenario/networks/turns/' + value + '_turns.in')
-        if my_project.tod in network_config['transit_tod_list']:
-            my_project.process_vehicles('inputs/scenario/networks/vehicles.txt')
-            my_project.process_transit('inputs/scenario/networks/transit/' + value + '_transit.in')
-            for att in network_config['transit_line_extra_attributes']:
-                my_project.create_extra_attribute('TRANSIT_LINE', att)
-            my_project.import_extra_attributes('inputs/scenario/networks/extra_attributes/' + value + '_link_attributes.in/extra_transit_lines_'+ str(tod_index[value]) +'.txt', False)
+
+        my_project.process_modes("inputs/scenario/networks/modes.txt")
+
+        my_project.process_base_network(
+            "inputs/scenario/networks/roadway/" + value + "_roadway.in"
+        )
+
+        my_project.process_shape(
+            "inputs/scenario/networks/shape/" + value + "_shape.in"
+        )
+        my_project.process_turn("inputs/scenario/networks/turns/" + value + "_turns.in")
+        if my_project.tod in network_config["transit_tod_list"]:
+            my_project.process_vehicles("inputs/scenario/networks/vehicles.txt")
+            my_project.process_transit(
+                "inputs/scenario/networks/transit/" + value + "_transit.in"
+            )
+            for att in network_config["transit_line_extra_attributes"]:
+                my_project.create_extra_attribute("TRANSIT_LINE", att)
+            my_project.import_extra_attributes(
+                "inputs/scenario/networks/extra_attributes/"
+                + value
+                + "_link_attributes.in/extra_transit_lines_"
+                + str(tod_index[value])
+                + ".txt",
+                False,
+            )
             update_headways(my_project, headway_df)
 
         print(value)
-        for att in network_config['link_extra_attributes']:
-            my_project.create_extra_attribute('LINK', att)
-        for att in network_config['node_extra_attributes']:
-            my_project.create_extra_attribute('NODE', att)
-        my_project.import_extra_attributes('inputs/scenario/networks/extra_attributes/' + value + '_link_attributes.in/extra_links_'+ str(tod_index[value]) +'.txt')
-        my_project.import_extra_attributes('inputs/scenario/networks/extra_attributes/' + value + '_link_attributes.in/extra_nodes_'+ str(tod_index[value]) +'.txt')
+        for att in network_config["link_extra_attributes"]:
+            my_project.create_extra_attribute("LINK", att)
+        for att in network_config["node_extra_attributes"]:
+            my_project.create_extra_attribute("NODE", att)
+        my_project.import_extra_attributes(
+            "inputs/scenario/networks/extra_attributes/"
+            + value
+            + "_link_attributes.in/extra_links_"
+            + str(tod_index[value])
+            + ".txt"
+        )
+        my_project.import_extra_attributes(
+            "inputs/scenario/networks/extra_attributes/"
+            + value
+            + "_link_attributes.in/extra_nodes_"
+            + str(tod_index[value])
+            + ".txt"
+        )
 
-        arterial_delay(my_project, network_config['rdly_factor'])
-        if config['add_distance_pricing']:
-            distance_pricing(config['distance_rate_dict'][value], my_project)
-       
+        arterial_delay(my_project, network_config["rdly_factor"])
+        if config["add_distance_pricing"]:
+            distance_pricing(config["distance_rate_dict"][value], my_project)
+
+
 def main():
+    run_importer(network_config["network_summary_project"])
 
-    run_importer(network_config['network_summary_project'])
-    
-    print('networks imported')
+    print("networks imported")
+
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
