255017b25bac89b6be426e43c757a14467e08cda
Joanneylin <71201494+Joanneylin@users.noreply.github.com>
2024-05-30 18:13:59 -0700

    add 2017/2019 survey

--- scripts/summarize/validation/validation_scripts/validation_data_input.py
+++ scripts/summarize/validation/validation_scripts/validation_data_input.py
@@ -7,7 +7,7 @@ import plotly.express as px
 config = toml.load(os.path.join(os.getcwd(), r'../../../../configuration', 'validation_configuration.toml'))
 
 # Read data for model and survey data
-def get_data(df_name, survey_data_path, col_list=None):
+def get_data(df_name, col_list=None, max_rows=None):
 
     if col_list is None:
         model_cols = None
@@ -17,13 +17,30 @@ def get_data(df_name, survey_data_path, col_list=None):
         survey_cols = col_list
 
     # model data
-    model = pd.read_csv(os.path.join(config['model_dir'], 'outputs', 'daysim', '_'+df_name+'.tsv'), sep='\t', usecols=model_cols)
+    model = pd.read_csv(os.path.join(config['model_dir'], 'outputs', 'daysim', '_'+df_name+'.tsv'),
+                        sep='\t', usecols=model_cols, nrows=max_rows)
     model['source'] = "model"
 
     # survey data
-    survey = pd.read_csv(os.path.join(survey_data_path, '_'+df_name+'.tsv'), sep='\t', usecols=model_cols)
+    if df_name == 'tour':
+        survey_path = config['tour_survey_dir']
+        survey_2017_path = config['tour_survey_2017_dir']
+    else:
+        survey_path = config['survey_dir']
+        survey_2017_path = config['survey_2017_dir']
+
+    survey = pd.read_csv(os.path.join(survey_path, '_'+df_name+'.tsv'),
+                         sep='\t', usecols=model_cols, nrows=max_rows)
     survey['source'] = "survey"
 
-    df = pd.concat([model, survey])
+    if config['include_2017_2019']:
+        # 2017 survey data
+        survey_2017 = pd.read_csv(os.path.join(survey_2017_path, '_'+df_name+'.tsv'),
+                                  sep='\t', usecols=model_cols, nrows=max_rows)
+        survey_2017['source'] = "survey (2017/2019)"
+        df = pd.concat([model, survey, survey_2017])
+
+    else:
+        df = pd.concat([model, survey])
 
     return df
