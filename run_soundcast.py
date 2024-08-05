9f9e376322833c264c1623e717d72f1b3a6b79a2
Joanneylin <71201494+Joanneylin@users.noreply.github.com>
2024-08-05 09:06:03 -0700

    bug fix for running validation summaries in run_soundcast

--- run_soundcast.py
+++ run_soundcast.py
@@ -214,8 +214,8 @@ def run_all_summaries():
         print(script)
         subprocess.call([sys.executable, os.path.join(base_path, script+'.py')])
     subprocess.run('conda activate summary && python scripts/summarize/standard/write_html.py', shell=True)
-    subprocess.call([sys.executable,'summarize/validation/create_network_validation.py'])
-    subprocess.run('python scripts/summarize/validation/create_validation_pages.py && conda deactivate', shell=True)
+    subprocess.run('conda activate summary && python summarize/validation/create_network_validation.py', shell=True)
+    subprocess.run('conda activate summary && python scripts/summarize/validation/create_validation_pages.py && conda deactivate', shell=True)
 
 def get_current_commit_hash():
     try:
