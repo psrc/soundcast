f7bb1e67f441b5e5a6efcf94d07b61438aec0fb8
Joanneylin <71201494+Joanneylin@users.noreply.github.com>
2024-10-25 13:44:55 -0700

    start summary notebook

---
+++ scripts/summarize/summary/create_summary_pages.py
@@ -0,0 +1,47 @@
+import os, sys, shutil
+from pathlib import Path
+import toml
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
+
+config = toml.load(Path.cwd()/'configuration/summary_configuration.toml')
+
+def run_ipynb(sheet_name, nb_path):
+    print("creating " + sheet_name + " summary")
+    with open(Path(nb_path) / (sheet_name + ".ipynb")) as f:
+        nb = nbformat.read(f, as_version=4)
+        if sys.version_info > (3, 0):
+            py_version = "python3"
+        else:
+            py_version = "python2"
+        ep = ExecutePreprocessor(timeout=1500, kernel_name=py_version)
+        ep.preprocess(nb, {"metadata": {"path": Path(nb_path)}})
+        with open(Path(nb_path) / (sheet_name + ".ipynb"), "wt") as f:
+            nbformat.write(nb, f)
+    print(sheet_name + " summary notebook created")
+
+
+def main():
+
+    # Try to remove existing data first
+    output_dir = Path.cwd() / config['p_output_dir'] / 'summary-notebook'
+    if os.path.exists(output_dir):
+        shutil.rmtree(output_dir)
+
+    for sheet_name in config["summary_list"]:
+        run_ipynb(sheet_name, 'scripts/summarize/summary/summary_scripts')
+
+    # render quarto book
+    # TODO: automate _quarto.yml chapter list
+    text = "quarto render scripts/summarize/summary"
+    os.system(text)
+    print("summary notebook created")
+
+    # Move these files to output folder
+    if not os.path.exists(Path.cwd() / config['p_output_dir']):
+        os.makedirs(Path.cwd() / config['p_output_dir'])
+    shutil.move(Path.cwd() / 'scripts/summarize/summary/summary-notebook', output_dir)
+
+
+if __name__ == "__main__":
+    main()
