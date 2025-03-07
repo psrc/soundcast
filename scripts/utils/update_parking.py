4b513edafcbfefc574c948fdb40de74f5c1e942b
Modeller <Modeller@psrc.local>
2025-03-07 15:27:42 -0800

    summary updates parking fixes

--- scripts/utils/update_parking.py
+++ scripts/utils/update_parking.py
@@ -21,7 +21,7 @@ import toml
 config = toml.load(os.path.join(os.getcwd(), "configuration/input_configuration.toml"))
 
 db_path = os.path.join("inputs/db", config["db_name"])
-input_parcels = r"inputs/scenario/landuse/parcels_urbansim.txt"
+input_parcels = r"outputs/landuse/parcels_urbansim.txt"
 
 # Load data
 conn = create_engine("sqlite:///" + db_path)
@@ -31,7 +31,7 @@ df_parking_cost = pd.read_sql(
     "SELECT * FROM parking_costs WHERE year==" + config["model_year"], con=conn
 )
 df_parcels = pd.merge(
-    left=df_parcels, right=df_parking_zones, left_on="TAZ_P", right_on="TAZ", how="left"
+    left=df_parcels, right=df_parking_zones, left_on="taz_p", right_on="TAZ", how="left"
 )
 
 # Join daily costs with parcel data
@@ -39,11 +39,11 @@ df_parking_cost = pd.merge(df_parcels, df_parking_cost, on="ENS", how="left")
 
 # Clean up the results and store in same format as original parcel.txt file
 df = pd.DataFrame(df_parking_cost)
-drop_columns = ["PPRICDYP", "PPRICHRP"]
-df = df.drop(drop_columns, 1)
+drop_columns = ["ppricdyp", "pprichrp"]
+df = df.drop(drop_columns, axis=1)
 
 for column_title in drop_columns:
-    df = df.rename(columns={"DAY_COST": "PPRICDYP", "HR_COST": "PPRICHRP"})
+    df = df.rename(columns={"DAY_COST": "ppricdyp", "HR_COST": "pprichrp"})
 
 
 # For parcels in regions with non-zero parking costs, ensure each parcel has some minumum parking spaces available
@@ -51,11 +51,11 @@ for column_title in drop_columns:
 # For now, replacing all zero-parking locations with region-wide average number of parking spots.
 # We can update this with an ensemble-wide average from GIS analysis later.
 
-avg_daily_spaces = df[df["PARKDY_P"] > 0]["PARKDY_P"].mean()
-avg_hourly_spaces = df[df["PARKHR_P"] > 0]["PARKHR_P"].mean()
+avg_daily_spaces = df[df["parkdy_p"] > 0]["parkdy_p"].mean()
+avg_hourly_spaces = df[df["parkhr_p"] > 0]["parkhr_p"].mean()
 
-daily_parking_spaces = df[df["PPRICDYP"] > 0]["PARKDY_P"]
-hourly_parking_spaces = df[df["PPRICHRP"] > 0]["PARKHR_P"]
+daily_parking_spaces = df[df["ppricdyp"] > 0]["parkdy_p"]
+hourly_parking_spaces = df[df["pprichrp"] > 0]["parkhr_p"]
 
 replace_daily_parking_spaces = daily_parking_spaces[daily_parking_spaces == 0]
 replace_hourly_parking_spaces = hourly_parking_spaces[hourly_parking_spaces == 0]
@@ -69,16 +69,16 @@ replace_hourly_parking_spaces = pd.DataFrame(replace_hourly_parking_spaces)
 # merge results back in to main data
 if len(replace_daily_parking_spaces) > 0:
     df = df.join(replace_daily_parking_spaces, lsuffix="_left", rsuffix="_right")
-    df = df.rename(columns={"PARKDY_P_right": "PARKDY_P"})
-    if "PARKDY_P_left" in df:
-        df = df.drop("PARKDY_P_left", 1)
+    df = df.rename(columns={"parkdy_p_right": "parkdy_p"})
+    if "parkdy_p_left" in df:
+        df = df.drop("parkdy_p_left", axis=1)
     df = df.join(replace_hourly_parking_spaces, lsuffix="_left", rsuffix="_right")
-    df = df.rename(columns={"PARKHR_P_right": "PARKHR_P"})
-    if "PARKHR_P_left" in df:
-        df = df.drop("PARKHR_P_left", 1)
+    df = df.rename(columns={"parkhr_p_right": "parkhr_p"})
+    if "parkhr_p_left" in df:
+        df = df.drop("parkhr_p_left", axis=1)
 
 # Delete TAZ and ENS (parking zone) columns to restore file to original form.
-df = df.drop(["ENS", "TAZ"], 1)
+df = df.drop(["ENS", "TAZ"], axis=1)
 
 # Make sure there are no Na's
 df.fillna(0, inplace=True)
